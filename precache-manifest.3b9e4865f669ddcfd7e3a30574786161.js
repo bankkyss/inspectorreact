5708f6725aabd295edf69f14c99f588ffea2e3e6
bankkyss <42342455+bankkyss@users.noreply.github.com>
2020-11-30 21:44:39 +0700

    Updates

---
+++ precache-manifest.3b9e4865f669ddcfd7e3a30574786161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96a80e4de1e06fbdb944696d5938e73",
+    "url": "/inspectorreact/index.html"
+  },
+  {
+    "revision": "f4bedef7270f9eb9b0ac",
+    "url": "/inspectorreact/static/css/main.8b85baac.chunk.css"
+  },
+  {
+    "revision": "a866ef98472b3b910abf",
+    "url": "/inspectorreact/static/js/2.6d421fed.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/inspectorreact/static/js/2.6d421fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4bedef7270f9eb9b0ac",
+    "url": "/inspectorreact/static/js/main.ea845492.chunk.js"
+  },
+  {
+    "revision": "0d84cb581ef2f47fc689",
+    "url": "/inspectorreact/static/js/runtime-main.6bae7daf.js"
+  }
+]);
